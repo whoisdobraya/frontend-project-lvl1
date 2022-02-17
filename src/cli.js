eb526de0133590a9f74408ce169a82f5bffe8921
Mary <masha_dobraya@mail.ru>
2022-02-17 16:05:35 +0300

    Update workflows

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,12 @@
 import readlineSync from 'readline-sync';
 
-export const getUserName = () => {
-	 const userName = readlineSync.question('May I have your name? ');
-	 if (userName === '') {
-		console.log(`Hello, anonymous!`);
-	 } else {
-		console.log(`Hello, ${userName}!`);
-	 }
+const getUserName = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  if (userName === '') {
+    console.log('Hello, anonymous!');
+  } else {
+    console.log(`Hello, ${userName}!`);
+  }
 };
+
+export default getUserName;
